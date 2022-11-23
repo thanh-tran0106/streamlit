65b2db1d4fffc8e1b411340356e8a50763e585b0
thanh-tran0106 <74903521+thanh-tran0106@users.noreply.github.com>
2022-11-23 23:19:31 +0200

    Update 5_🛠_Demo.py

--- pages/5_🛠_Demo.py
+++ pages/5_🛠_Demo.py
@@ -2,12 +2,21 @@ import streamlit as st
 
 st.title("Demo")
 
+tab1, tab2 = st.tabs(["TOPOLOGY", "LINKS"])
 
-from PIL import Image
-topo1 = Image.open('/app/pages/topo1.png')
+with tab1:
+  st.header("TOPOLOGY")
+  st.markdown("- Abstracts a collection of tasks")
+  
+  from PIL import Image
+  topo1 = Image.open('/app/pages/topo1.png')
+  st.image(topo1)
 
-st.image(topo1)
-
-topo2 = Image.open('/app/pages/topo2.png')
-
-st.image(topo2)
+  topo2 = Image.open('/app/pages/topo2.png')
+  st.image(topo2)
+  
+with tab2:
+  st.header("LINKS")
+  import streamlit as st 
+  st.write("Swarmpit [link](http://docker.demo.ai/swarmpit)")
+  st.write("Jenkins [link](http://jenkins.docker.demo.ai/)")
