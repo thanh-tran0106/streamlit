6a79856c43fc2d946c4353180ae1dfbaf0b9c684
thanh-tran0106 <74903521+thanh-tran0106@users.noreply.github.com>
2022-11-23 13:20:38 +0200

    snowflake

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,27 @@ If you have any questions, checkout our [my code](https://github.com/thanh-tran0
 
 In the meantime, sit back and enjoy the demo :) 
 """
+
+# Use local CSS
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+local_css("style/style.css")
+
+# Load Animation
+animation_symbol = "❄"
+
+st.markdown(
+    f"""
+    <div class="snowflake">{animation_symbol}</div>
+    <div class="snowflake">{animation_symbol}</div>
+    <div class="snowflake">{animation_symbol}</div>
+    <div class="snowflake">{animation_symbol}</div>
+    <div class="snowflake">{animation_symbol}</div>
+    <div class="snowflake">{animation_symbol}</div>
+    <div class="snowflake">{animation_symbol}</div>
+    <div class="snowflake">{animation_symbol}</div>
+    <div class="snowflake">{animation_symbol}</div>
+    """,
+    unsafe_allow_html=True,
+)
