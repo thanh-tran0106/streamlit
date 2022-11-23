ba98637edee5e72fddf7e4b296f5851b20543fa3
thanh-tran0106 <74903521+thanh-tran0106@users.noreply.github.com>
2022-11-23 16:12:10 +0200

    Update 3_🛂_Control_Plane.py

--- pages/3_🛂_Control_Plane.py
+++ pages/3_🛂_Control_Plane.py
@@ -6,3 +6,13 @@ st.header("SWIM PROTOCOL")
 st.markdown("- Ask neighbor A if they're still alive")
 st.markdown("- Ask three other neighbors if they can reach neighbor A")
 st.markdown("- No to both: conclude neighbor A is unhealthy/dead")
+
+
+fakeimage = "https://c.tenor.com/T4HzdRJwVQIAAAAC/swim-in.gif"
+st.image(fakeimage, caption='SWIM')
+
+st.markdown("Just kidding, this is the real one ^^")
+
+image = "https://www.brianstorti.com/assets/images/swim/failure-detection.png"
+#
+st.image(image, caption='Failure Detection')
