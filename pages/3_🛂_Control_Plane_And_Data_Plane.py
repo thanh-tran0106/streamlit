8dd8f85fcc9fcac7bce886e6cdaae64129077523
thanh-tran0106 <74903521+thanh-tran0106@users.noreply.github.com>
2022-11-23 16:35:06 +0200

    Update and rename 3_🛂_Control_Plane.py to 3_🛂_Control_Plane_And_Data_Plane.py

--- pages/3_🛂_Control_Plane_And_Data_Plane.py
+++ pages/3_🛂_Control_Plane_And_Data_Plane.py
@@ -21,3 +21,13 @@ st.header("GOSSIP CONTROL PLANE")
 st.markdown("- Docker control plane: piggyback DNS info on SWIM traffic")
 st.markdown("- DNS entries spread 'infectiously' through cluster.")
 st.markdown("- Gossip control plane scales independent of cluster size")
+
+dataplaneimage = "https://blog.octo.com/wp-content/uploads/2017/08/controlplane.png"
+vxlanimage = "https://blog.octo.com/wp-content/uploads/2017/08/packetwalkvxlan.png"
+st.header("DATA PLANE")
+st.markdown("- This includes container traffic and traffic to and from external clients.")
+st.image(dataplaneimage, caption='Control Plane')
+
+st.markdown("- It uses standard VXLAN to encapsulate container traffic and send it to other containers")
+st.markdown("- VXLAN is an encapsulation format that wraps Layer 2 segments with an IP/UDP header, and then send it over Layer 3 networks")
+st.image(vxlanimage, caption='Networking on Control Plan')
