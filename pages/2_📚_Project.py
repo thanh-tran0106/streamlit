0bea1edf9d6f3685290331781b20c7d85bf4686c
thanh-tran0106 <74903521+thanh-tran0106@users.noreply.github.com>
2022-11-23 14:52:19 +0200

    Update 2_📚_Project.py

--- pages/2_📚_Project.py
+++ pages/2_📚_Project.py
@@ -1,3 +1,13 @@
 import streamlit as st
 
 st.title("Docker Swarm")
+
+st.header("DISTRIBUTED APPLICATION ARCHITECTURE")
+st.markdown("- Applications consisting of one or more containers across one or more nodes")
+st.markdown("- Docker Swarm facilitates multi-node design.")
+st.markdown("- Also supports multiple interacting services (like Compose)")
+
+st.header("NETWORKING REQUIREMENTS")
+st.markdown("- Control plane: enable service discovery for containers across hosts")
+st.markdown("- Data plane: enable moving a packet from host A to host B")
+st.markdown("- Management plane: decide where containers should be scheduled")
