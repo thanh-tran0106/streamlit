78bb4f605ad0ea4e973a5b0e57496e666273072e
thanh-tran0106 <74903521+thanh-tran0106@users.noreply.github.com>
2022-11-23 15:58:54 +0200

    Delete pages/controlplane directory

--- pages/controlplane/1_controlplane.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-st.title("CONTROL PLANE")
-
-st.header("SWIM PROTOCOL")
-st.markdown("- Ask neighbor A if they're still alive")
-st.markdown("- Ask three other neighbors if they can reach neighbor A")
-st.markdown("- No to both: conclude neighbor A is unhealthy/dead")
